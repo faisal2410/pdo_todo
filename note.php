5137495b378e1cb64c53ba7885eb4a526814c911
RaihanaFerdousi <raihanaferdousi110@gmail.com>
2024-06-28 20:51:32 +0600

    nahian bhai's conceptual class resource added

--- note.php
+++ note.php
@@ -4,3 +4,12 @@ Chatgpt Link about the project :
 
 https://chatgpt.com/share/5e1f495d-958c-4062-b51b-15dc7d737346
 */ 
+
+/*
+
+Nahian bhai's Conceptual class resource :
+
+https://alnahian2003.notion.site/Conceptual-Class-2-27-06-2024-24e7bba06a2647c9bfc57ced7e8eb9b5
+
+
+*/ 
